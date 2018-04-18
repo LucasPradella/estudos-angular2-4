3114b6ca3174ca9f0a39112564ab3244ebafb04b
Lucas Pradella <lucas.es.pradella@gmail.com>
2018-04-18 14:05:14 -0300

    Adicionando exemplos typescript x javaScript

---
+++ hello-typescript/main.ts
@@ -0,0 +1,28 @@
+var minhaVar = 'minha variavel'   // javaScript
+
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+
+let num = 2;      // ES 6 ou ES 2015
+const PI = 3.14;  // ES 6 ou ES 2015
+
+
+var numeros = [1, 2, 3];
+numeros.map(function (valor){
+    return valor * 2;
+});
+
+numeros.map(valor=>valor*2) // ES 6 ou ES 2015
+
+
+class Matematica {
+  soma(x, y){
+    return x+y;
+  }
+}
+
+
+var n1: String = 'Lucas'; // tipagem de variavel
